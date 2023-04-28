c77ebb89b694b3a3d3b4fa56e7948fee5570618d
Lakshman Karunasena <lakkaru@gmail.com>
2023-04-28 16:33:39 +0530

    feat- input values collected with onChange event to update variables.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import StudentForm from './StudentForm';
 
--- src/StudentForm.js
+++ src/StudentForm.js
@@ -1,26 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function StudentForm() {
+
+    const [name, setName]=useState();
+    const [nic, setNIC]=useState();
+    const [email, setEmail]=useState();
+    const [phone, setPhone]=useState();
+
+    const handleChanges=()=>{
+        console.log(name);
+        console.log(nic);
+        console.log(email);
+        console.log(phone);
+        
+    }
+
   return (
     <div style={{ marginTop: "20px" }}>
       <div style={{ marginBottom: "15px" }}>
         <label htmlFor="name">Name : </label>
-        <input id="name" />
+        <input id="name" onChange={(e)=>setName(e.target.value)}/>
       </div>
       <div style={{ marginBottom: "15px" }}>
         <label htmlFor="nic">NIC : </label>
-        <input id="nic" />
+        <input id="nic" onChange={(e)=>setNIC(e.target.value)}/>
       </div>
       <div style={{ marginBottom: "15px" }}>
         <label htmlFor="email">Email : </label>
-        <input id="email" />
+        <input id="email" onChange={(e)=>setEmail(e.target.value)}/>
       </div>
       <div style={{ marginBottom: "15px" }}>
         <label htmlFor="phone">Phone : </label>
-        <input id="phone" />
+        <input id="phone" onChange={(e)=>setPhone(e.target.value)}/>
       </div>
       <div>
-        <button>Submit</button>
+        <button onClick={handleChanges}>Submit</button>
       </div>
     </div>
   );
